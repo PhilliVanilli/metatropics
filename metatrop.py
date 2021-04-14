5cc8e7ffe388f351c8d08509bf62255c1dca3a1e
PhilliVanilli <philippeselhorst@gmail.com>
2021-04-14 11:27:36 +0200

    ...

--- metatrop.py
+++ metatrop.py
@@ -11,7 +11,6 @@ from basecall_guppy import main as gupppy_basecall
 from demultiplex_guppy import main as guppy_demultiplex
 from src.misc_functions import try_except_continue_on_fail
 from src.misc_functions import try_except_exit_on_fail
-from src.misc_functions import py3_fasta_iter
 from src.misc_functions import cat_sample_names
 from src.misc_functions import filter_length_trim_seq
 from src.misc_functions import fasta_to_dct
@@ -22,19 +21,19 @@ class Formatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpForm
     pass
 
 def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth, run_step,
-         rerun_step_only, basecall_mode, msa_cons, artic, cpu_threads,
+         rerun_step_only, basecall_mode, cpu_threads,use_gaps,
          guppy_dir, real_time):
 
     # set the dir paths
     script_dir = Path(__file__).absolute().parent
-    reference_seqs_file = Path(script_dir, "references.fasta")
     project_dir = Path(project_dir).absolute()
+    reference_seqs_file = Path(script_dir, "references.fasta")
     print(f"\nProject dir is {project_dir}")
     run_name = project_dir.parts[-1]
     fast5_dir = Path(project_dir, "fast5")
     fastq_dir = Path(project_dir, "fastq")
     demultiplexed_dir = Path(project_dir, "demultiplexed")
-    sample_dir = Path(project_dir, "samples")
+    all_sample_dir = Path(project_dir, "samples")
     sample_names_file = Path(project_dir, "sample_names.csv")
     if not sample_names_file:
         sys.exit("Could not find sample_names.csv in project dir")
@@ -44,12 +43,10 @@ def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth
             sys.exit("Could not find sequencing_summary*.txt in project dir")
         else:
             seq_summary_file = file
-    plot_dir = Path(project_dir, "seq_depth_plots")
-    if os.path.exists(plot_dir):
-        shutil.rmtree(plot_dir)
-    plot_dir.mkdir(mode=0o777, parents=True, exist_ok=True)
+
     time_stamp = str('{:%Y-%m-%d_%Hh%M}'.format(datetime.datetime.now()))
     log_file = Path(project_dir, f"{time_stamp}_{run_name}_log_file.txt")
+    log_file_final = Path(project_dir, f"{time_stamp}_{run_name}_log_file_final.txt")
 
     # log start time and change dir to project dir
     print(f"\n# Starting metagenomics pipeline for project: {run_name}\n")
@@ -93,7 +90,7 @@ def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth
         else:
             sys.exit("Demultiplexing failed")
 
-    # length filtering allowing for multiple fastqs from multiple exp per barcode
+    # length filtering and primer trimming allowing for multiple fastqs from multiple exp per barcode
     if run_step == 2:
         pre_existing_files = list(demultiplexed_dir.glob("*.fastq"))
         if pre_existing_files:
@@ -115,18 +112,18 @@ def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth
                 cat_cmd += f" > {concat_outfile}"
                 try_except_exit_on_fail(cat_cmd)
                 new_name = Path(demultiplexed_dir, f"{run_name}_{barcode_number}.fastq")
-                filtered_file = filter_length_trim_seq(concat_outfile, new_name, max_len, min_len, 10, 10)
+                filtered_file = filter_length_trim_seq(concat_outfile, new_name, max_len, min_len, 27, 27)
                 os.unlink(str(concat_outfile))
                 if not filtered_file:
-                    print(f"No sequences in file after length filtering and trimming for {concat_outfile}\n")
+                    print(f"No sequences in file after length filtering and primer trimming for {concat_outfile}\n")
             else:
-                print(f"Length filtering and trimming {folder}")
+                print(f"Length filtering and primer trimming {folder}")
                 file = Path(search[0])
                 barcode_number = file.parent.parts[-1]
                 new_name = Path(demultiplexed_dir, f"{run_name}_{barcode_number}.fastq")
-                filtered_file = filter_length_trim_seq(file, new_name, max_len, min_len, 10, 10)
+                filtered_file = filter_length_trim_seq(file, new_name, max_len, min_len, 27, 27)
                 if not filtered_file:
-                    print(f"No sequences in file after length filtering and trimming for {file}\n")
+                    print(f"No sequences in file after length filtering and primer trimming for {file}\n")
         if not rerun_step_only:
             run_step = 3
         elif rerun_step_only:
@@ -147,16 +144,16 @@ def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth
         sample_names_dict = sample_names_df.set_index('sample_name').T.to_dict(orient='list')
 
         for sample_name, [barcode_1, barcode_2] in sample_names_dict.items():
-            sample_dir = Path(sample_dir, sample_name)
-            if not sample_dir.exists():
-                Path(sample_dir).mkdir(mode=0o777, parents=True, exist_ok=True)
+            all_sample_dir = Path(all_sample_dir, sample_name)
+            if not all_sample_dir.exists():
+                Path(all_sample_dir).mkdir(mode=0o777, parents=True, exist_ok=True)
             barcode_1_file = Path(demultiplexed_dir, barcode_1)
             # allow for case where only one barcode was specified per sample.
             if barcode_2 == " ":
                 barcode_2_file = ""
             else:
                 barcode_2_file = Path(demultiplexed_dir, barcode_2)
-            cat_outfile = Path(sample_dir, f"{sample_name}.fastq")
+            cat_outfile = Path(all_sample_dir, f"{sample_name}.fastq")
             cat_cmd = f"cat {str(barcode_1_file)} {str(barcode_2_file)} > {cat_outfile}"
             print(cat_cmd)
             with open(log_file, "a") as handle:
@@ -172,56 +169,119 @@ def main(project_dir, reference, ref_start, ref_end, min_len, max_len, min_depth
         else:
             sys.exit("Run step only completed, exiting")
 
-    # # concatenate multiple barcodes per sample and rename according to sample file
-    # if run_step == 4:
-    #     print("Running variant calling on samples")
-    #     with open(log_file, "a") as handle:
-    #         handle.write(f"\nRunning variant calling on samples\n")
-    #
-    #     all_sample_files = Path(sample_dir).glob("*/*.fastq")
-    #
-    #
-    #
-    #     # initialize the file, and add reference to all consensus file
-    #     with open(all_samples_consens_seqs, 'w') as fh:
-    #         fh.write(f">{ref_name}\n{ref_seq}\n")
-    #     p = pathlib.Path(project_path, project_name + '_mapping.csv')
-    #     with open(p, 'w') as fh:
-    #         fh.close()
-    #
-    #     samples_run = 1
-    #     old_number_png_files = 0
-    #     for sample_fastq in all_sample_files:
-    #         # get folder paths
-    #         sample_folder = pathlib.Path(sample_fastq).parent
-    #         sample_name = pathlib.Path(sample_fastq).stem
-    #         os.chdir(sample_folder)
-    #         seq_summary_file_name = ""
-    #         for file in project_path.glob('sequencing_summary*.txt'):
-    #             seq_summary_file_name = file
-    #         seq_summary_file = pathlib.Path(seq_summary_file_name).resolve()
-    #         artic_folder = pathlib.Path(sample_folder, "artic")
-    #         if os.path.exists(artic_folder):
-    #             shutil.rmtree(artic_folder)
-    #         artic_folder.mkdir(mode=0o777, parents=True, exist_ok=True)
-    #
-    #         # check if fastq is present
-    #         if not sample_fastq.is_file():
-    #             print(f"\nCould not find the concatenated sample fastq file: {sample_fastq}\nskipping sample")
-    #             with open(log_file, "a") as handle:
-    #                 handle.write(
-    #                     f"\nCould not find the concatenated sample fastq file: {sample_fastq}\nskipping sample")
-    #             continue
-    #         print(f"\n________________\n\nStarting processing sample: {sample_name}\n________________\n")
-    #         with open(log_file, "a") as handle:
-    #             handle.write(
-    #                 f"\n________________\n\nStarting processing sample: {sample_name}\n________________\n")
+    # Reference-based assembly
+    if run_step == 4:
+        print("Starting reference-based assembly ")
+        with open(log_file, "a") as handle:
+            handle.write(f"\nStarting reference-based assembly\n")
+
+        # get number of samples and threads
+        number_samples = (len(list(all_sample_dir.glob('*/*.fastq'))))
+        print("number of samples=" + str(number_samples))
+        max_threads = cpu_threads
+        used_threads = 0
+        msa_threads = 6
+
+        # delete pre existing files
+        for file in Path(all_sample_dir).glob("*/*.*"):
+            if not str(file).endswith(".fastq"):
+                os.remove(file)
+
+        log_file_msa_temp = Path(project_dir, f"{time_stamp}_{run_name}_log_file_msa_temp.txt")
+        log_file_msa = Path(project_dir, f"{time_stamp}_{run_name}_log_file_msa.txt")
+
+        print(f"\n________________\n\nStarting MSA processing samples\n________________\n")
+        print(f"min_depth = {min_depth}")
+        with open(log_file_msa_temp, "a") as handle:
+            handle.write(
+                f"\n________________\n\nStarting MSA processing samples\n________________\n")
+            handle.write(f"\nmin_depth = {min_depth}\n")
+
+        all_sample_files = Path(all_sample_dir).glob("*/*.fastq")
+        sample_no = 0
+        for sample_fastq in all_sample_files:
+            sample_no += 1
+
+            # get fastq path and name
+            sample_dir = Path(sample_fastq).parent
+            sample_name = Path(sample_fastq).stem
+            log_file_msa_sample = Path(project_dir, f"{time_stamp}_{sample_name}_log_file_msa_sample.txt")
+            os.chdir(sample_dir)
+
+            # check free threads
+            finished_threads = len(list(Path(sample_dir).glob("*/*_msa_consensus.fasta")))
+            free_threads = max_threads + finished_threads - used_threads
+            print("\nfree_threads = " + str(free_threads))
+            print('\n' + 'waiting for free threads')
+            while free_threads < msa_threads:
+                time.sleep(5)
+                finished_threads = len(list(Path(sample_dir).glob("*/*_msa_consensus.fasta")))
+                free_threads = max_threads + finished_threads - used_threads
+
+            # check if fastq is present
+            file_present = list(sample_dir.glob("*.fastq"))
+            if not file_present:
+                print(
+                    f"\nCould not find the concatenated sample fastq file in sample folder: {sample_fastq}\nskipping sample")
+                with open(log_file_msa_sample, "a") as handle:
+                    handle.write(
+                        f"\nCould not find the concatenated sample fastq file in sample folder: {sample_fastq}\nskipping sample")
+                continue
+            print(f"\n------->Running majority consensus pipeline for {sample_no} st/nd sample {sample_name} in new window\n")
+            with open(log_file_msa_sample, "a") as handle:
+                handle.write(
+                    f"\n\n------->Running majority consensus pipeline for {sample_no} st/nd sample {sample_name} in new window\n")
+
+            # start majority consensus pipeline in new window
+            majority_cmd = f"python ~/nanopore_pipeline_wrapper/msa_consensus.py -in {sample_fastq} -lf {log_file_msa_sample} " \
+                           f"-rs {reference_seqs_file} " \
+                           f"-t {msa_threads} -d {min_depth} {use_gaps}"
+            print(majority_cmd)
+            try_except_continue_on_fail(f"gnome-terminal -- /bin/sh -c 'conda run -n nanop {majority_cmd}'")
+            used_threads += msa_threads
+
+        # concat all log files
+        os.chdir(project_dir)
+        loglist_msa = []
+        for path in Path(project_dir).glob("*_log_file_msa_sample.txt"):
+            loglist_msa.append(str(path))
+        sep = " "
+        string_msa = sep.join(loglist_msa)
+        cat_cmd = f"cat {str(log_file_msa_temp)} {string_msa} > {log_file_msa}"
+        try_except_continue_on_fail(cat_cmd)
+        cat_cmd = f"cat {str(log_file)} {str(log_file_msa)} > {log_file_final}"
+        try_except_continue_on_fail(cat_cmd)
+        for path in list(Path(project_dir).glob("*_log_file_msa_sample.txt")):
+            os.remove(path)
+        os.remove(log_file_msa_temp)
+        os.remove(log_file_msa)
+        os.remove(log_file)
+        os.remove(log_file)
+
+    # print end time
+    now = datetime.datetime.now()
+    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+    print(f"\nend time = {date_time}\n\n")
+    with open(log_file_final, "a") as handle:
+        handle.write(f"\nend time = {date_time}\n\n")
+
+    print("Sample processing completed\n")
+    with open(log_file_final, "a") as handle:
+        handle.write(f"\nSample processing completed\n\n")
+
+    # compress fast5 files
+    targzpath = Path(project_dir.parent, run_name + ".tar.gz")
+    tarcmd = f"tar cf - {fast5_dir} | pigz -7 -p 16  > {targzpath}"
+    try_except_exit_on_fail(tarcmd)
+    print(tarcmd)
+    with open(log_file_final, "a") as handle:
+        handle.write(f"\n{tarcmd}\n\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process raw nanopore reads to fasta consensus sequences",
                                      formatter_class=Formatter)
 
-    parser.add_argument("-in", "--project_path", default=argparse.SUPPRESS, type=str,
+    parser.add_argument("-in", "--project_dir", default=argparse.SUPPRESS, type=str,
                         help="The path to the directory containing the 'fast5' and 'fastq' dirs ", required=True)
     parser.add_argument("-r", "--reference", type=str, help="The reference genome and primer scheme to use",
                         choices=["ChikAsian_V1_400", "ChikECSA_V1_800", "ZikaAsian_V1_400", "SARS2_V1_800", "SARS2_V1_400", "DENV1_V1_400", "DENV2_V1_400"], required=False)
@@ -244,18 +304,16 @@ if __name__ == "__main__":
                              "--run_step 1 = demultiplex with Guppy\n"
                              "--run_step 2 = size filer and rename demultiplexed fastq file\n"
                              "--run_step 3 = concatenate demultiplexed files into sample files\n"
-                             "--run_step 4 = run read mapping and all the variant calling steps on each sample\n")
+                             "--run_step 4 = run reference-based viral genome assembly on each sample\n")
     parser.add_argument("--run_step_only", default=False, action="store_true",
                         help="Only run the step specified in 'run_step'", required=False)
     parser.add_argument("-b", "--basecall_mode", default=1, choices=[0, 1], type=int,
                         help="0 = basecall in fast mode\n"
                              "1 = basecall in high accuracy mode\n", required=False)
-    parser.add_argument("-m", "--msa", default=False, action="store_true",
-                        help="Generate consensus from MSA", required=False)
-    parser.add_argument("-a", "--art", default=False, action="store_true",
-                        help="Generate consensus with Artic pipeline", required=False)
     parser.add_argument("-c", "--cpu_threads", type=int, default=16, choices=range(0, 16),
-                        help="The number of cpu threads to use for bwa, nanopolish etc...", required=False)
+                        help="The number of cpu threads to use", required=False)
+    parser.add_argument("-ug", "--use_gaps", default=False, action="store_true",
+                        help="use gap characters when making the consensus sequences", required=False)
     parser.add_argument("-p", "--guppy_path", default=argparse.SUPPRESS, type=str,
                         help="The path to the guppy executables eg: '.../ont-guppy/bin/'", required=True)
     parser.add_argument("-rt", "--real_time", default=False, action="store_true",
@@ -263,7 +321,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    project_path = args.project_path
+    project_dir = args.project_dir
     reference = args.reference
     reference_start = args.reference_start
     reference_end = args.reference_end
@@ -273,11 +331,10 @@ if __name__ == "__main__":
     run_step = args.run_step
     run_step_only = args.run_step_only
     basecall_mode = args.basecall_mode
-    msa_cons = args.msa
-    artic = args.art
     cpu_threads = args.cpu_threads
+    use_gaps = args.use_gaps
     guppy_path = args.guppy_path
     real_time = args.real_time
 
-    main(project_path, reference, reference_start, reference_end, min_len, max_len, min_depth, run_step,
-         run_step_only, basecall_mode, msa_cons, artic, cpu_threads, guppy_path, real_time)
+    main(project_dir, reference, reference_start, reference_end, min_len, max_len, min_depth, run_step,
+         run_step_only, basecall_mode, cpu_threads, use_gaps, guppy_path, real_time)
